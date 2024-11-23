04fbe2f04d52cad865f9a9b544e542decb971d30
lhx <1194733285@qq.com>
2024-11-23 14:17:09 +0800

    feat: 泛型

---
+++ src/fanxing.ts
@@ -0,0 +1,23 @@
+function log <T>(value:T):T {
+    console.log(value)
+    return value
+}
+
+log<number>(5)
+log<number[]>([1,2,3,4,5])
+//泛型接口
+interface Log<T=string>{
+    (value:T):T // 泛型接口的默认类型是string
+}
+const log2:Log<number>=log
+// 泛型类
+class Log2<T>{
+    run(value:T):T{
+        console.log(value)
+        return value
+    }
+}
+const log3=new Log2<number>()
+log3.run(5)
+
+
